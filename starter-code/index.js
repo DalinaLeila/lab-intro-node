a490fcf99c3ce8aa0e7d726f3e5227425f3f3c13
Dalina Weidinger <dalina.weidinger@gmail.com>
2019-11-05 11:35:06 +0100

    recap for TA

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,10 +1,37 @@
-var SortedList = function() {};
-
-SortedList.prototype.add     = function(item) {}
-SortedList.prototype.get     = function(pos) {}
-SortedList.prototype.max     = function() {}
-SortedList.prototype.min     = function() {}
-SortedList.prototype.average = function() {}
-SortedList.prototype.sum     = function() {}
+class SortedList {
+  constructor() {
+    this.items = [];
+  }
+  get length() {
+    return this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    return this.items;
+  }
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
+  max() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return this.items[this.length - 1];
+  }
+  min() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return this.items[0];
+  }
+  average() {
+    if (this.length <= 0) throw new Error("EmptySortedList");
+    return this.sum() / this.length;
+  }
+  sum() {
+    return this.length <= 0 ? 0 : this.items.reduce((a, b) => a + b, 0);
+  }
+}
 
 module.exports = SortedList;
